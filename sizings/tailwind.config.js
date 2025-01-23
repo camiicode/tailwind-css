66ee1e0ace4f792196d6900e8a6e64113f6859e0
camiicode <dftp93@gmail.com>
2025-01-22 20:39:06 -0500

    [2201202501] Update new section - Sizings

---
+++ sizings/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#91516B",
+        "secondary-color": "#86A2C2",
+        "tertiary-color": "#E9E8E9"        
+      }
+    },
+  },
+  plugins: [],
+}
